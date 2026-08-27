2f5ead6ab2d4d167ddb9cfcf4e5cec0a4055a7d7
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-27 09:27:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.60%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int n = x;
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return (x % sum == 0) ? sum : -1;
+    }
+};
