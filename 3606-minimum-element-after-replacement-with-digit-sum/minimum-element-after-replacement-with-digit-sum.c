057cab0688076c12b49870fda1654bb387a7ddc0
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-30 22:25:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (13.10%) - LeetSync

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.c
@@ -0,0 +1,21 @@
+int minElement(int* nums, int numsSize) {
+    int digit, temp, i, sum;
+    int min = 1000000;
+
+    for(i = 0; i < numsSize; i++) {
+        temp = nums[i];
+        sum = 0;
+
+        while(temp > 0) {
+            digit = temp % 10;
+            sum = sum + digit;
+            temp = temp / 10;
+        }
+
+        if(sum < min) {
+            min = sum;
+        }
+    }
+
+    return min;
+}
