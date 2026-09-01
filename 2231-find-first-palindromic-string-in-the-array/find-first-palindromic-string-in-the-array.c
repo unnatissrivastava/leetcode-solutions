df903cae8ad7a95951000a3f33f6e7f01c6d61ff
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-09-01 15:55:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (38.48%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.c
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.c
@@ -1,19 +1,20 @@
-char* firstPalindrome(char** words, int wordsSize) {
-    for (int i = 0; i < wordsSize; i++) {
-        int l = 0;
-        int h = strlen(words[i]) - 1;
-
-        while (l < h) {
-            if (words[i][l] != words[i][h])
-                break;
-
-            l++;
-            h--;
+bool isPalindrome(char*word){
+    int length = strlen(word);
+    int l = 0, h = length - 1;
+    while (l<h){
+        if (word[l] != word[h]){
+            return false;
         }
-
-        if (l >= h)
-            return words[i];
+        l++;
+        h--;
     }
-
+    return true;
+}
+char* firstPalindrome(char** words, int wordsSize) {
+    for (int i=0; i<wordsSize; i++){
+        if(isPalindrome(words[i])){
+            return words[i];
+        }
+    }
     return "";
 }
