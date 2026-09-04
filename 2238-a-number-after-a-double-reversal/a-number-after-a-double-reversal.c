1254fbcd750b8c7bc0bf76fd5d3530833cd80c1f
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-09-04 22:44:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (63.74%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.c
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.c
@@ -1,23 +1,19 @@
 bool isSameAfterReversals(int num) {
     int temp, digit;
-    int reverse1 = 0;
-    int reverse2 = 0;
+    int r1 = 0;
+    int r2 = 0;
     int original = num;
     temp = num;
-
-    while (temp > 0) {
-        digit = temp % 10;
-        reverse1 = reverse1 * 10 + digit;
-        temp = temp / 10;
+    while(temp>0){
+        digit = temp%10;
+        r1 = r1 * 10 + digit;
+        temp /= 10;
     }
-
-    temp = reverse1;
-
-    while (temp > 0) {
-        digit = temp % 10;
-        reverse2 = reverse2 * 10 + digit;
-        temp = temp / 10;
+    temp = r1;
+    while(temp>0){
+        digit = temp%10;
+        r2 = r2*10 + digit;
+        temp /= 10;
     }
-
-    return reverse2 == original;
+    return r2 == original;
 }
