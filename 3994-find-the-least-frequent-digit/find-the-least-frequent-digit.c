97b556e5389ec1cdafa11687a8194f997005054f
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-09-04 14:45:21 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (90.38%) - LeetSync

--- 3994-find-the-least-frequent-digit/find-the-least-frequent-digit.c
+++ 3994-find-the-least-frequent-digit/find-the-least-frequent-digit.c
@@ -1,15 +1,15 @@
 int getLeastFrequentDigit(int n) {
-    int digit, d, result, min;
+    int digit, d;
     int count[10] = {0};
-    while (n>0){
-        digit = n % 10;
+    while(n>0){
+        digit = n%10;
         count[digit]++;
-        n /= 10;
+        n/=10; 
     }
-    result = -1;
-    min = 100;
-    for(d = 0; d<=9 ;d++){
-        if(count[d]>0 && count[d]<min){
+    int result = -1;
+    int min = 100;
+    for(d=0; d<=9; d++){
+        if (count[d]>0 && count[d]<min){
             min = count[d];
             result = d;
         }
